91e9e9a051d5483f1be49a807a2b47b81cf765e7
Merwane <75220314+merwane-zioui@users.noreply.github.com>
2024-03-26 16:34:42 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,13 +4,6 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: "0.8.24",
-	networks: {
-		sepolia: {
-			url: process.env.SEPOLIA_URL,
-			accounts: [process.env.SEPOLIA_PRIVATE_KEY],
-			chainId: 11155111,
-		},
-	},
 	paths: {
 		artifacts: './front-application/src/artifacts'
 	}
